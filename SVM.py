85c3ef6245d40676faa47660a2ab0888db67b0db
FlyingCatCreatures <117092071+FlyingCatCreatures@users.noreply.github.com>
2024-02-19 13:35:56 +0100

    asd

--- SVM.py
+++ SVM.py
@@ -18,7 +18,7 @@ TrueStart=time.time()
 #Load data
 start = time.time()
 print("Loading Data")
-dataset = pd.read_csv("Short.csv") #Not included in github repository as filesize is over 100MB.
+dataset = pd.read_csv("DatsetFraud.csv") #Not included in github repository as filesize is over 100MB.
 end = -1*(start-time.time())
 print("Data loaded in " + str(end) + " seconds\n")
 
@@ -48,62 +48,72 @@ print("Features extracted from dataset in " + str(end) + " seconds\n")
 
 #Split data
 #features_train, features_test, labels_train, labels_test = train_test_split(data_features, data_labels, test_size=1-(TRAINING_SIZE/6362599), random_state=507, shuffle=True) #Random_state is just a seed value. Can be any number
-features_train, features_test, labels_train, labels_test = train_test_split(data_features, data_labels, test_size=0.2, random_state=507, shuffle=True) #Random_state is just a seed value. Can be any number
+features_train, features_test, labels_train, labels_test = train_test_split(data_features, data_labels, test_size=0.2, random_state=19274, shuffle=True) #Random_state is just a seed value. Can be any number
 
-i=0
-array=[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-while i<81 :
-    CLASS_WEIGHTS = {0:1.0, 1:0.05*i + 53}
+CLASS_WEIGHTS = {0:1.0, 1:52.04}
 
-    # Making the SVM Classifer
-    Classifier = SVC(kernel="rbf", verbose=False, cache_size=4096, class_weight=CLASS_WEIGHTS)
-    #Classifier = SVC(kernel="sigmoid", class_weight="balanced", verbose=True, degree=10, cache_size=4096)
+# Making the SVM Classifer
+Classifier = joblib.load("./Classifier (SVM).joblib")
+#Classifier = SVC(kernel="sigmoid", class_weight="balanced", verbose=True, degree=10, cache_size=4096)
 
-    # Training the model 
-    start = time.time()
-    print("Fitting SVM")
-    Classifier.fit(features_train, labels_train)
-    end =  -1*(start-time.time())
-    print("SVM fit in " + str(end) + " seconds on i=" + str(i))
 
-    #start = time.time()
-    #print("Calculating metrics")
-    # Using the model to predict the labels of the test data
-    prediction = Classifier.predict(features_test)
 
-    # Evaluating the accuracy of the model using the sklearn functions
-    accuracy = accuracy_score(labels_test,prediction)*100
-    precision = precision_score(labels_test,prediction, zero_division=0)*100
-    recall = recall_score(labels_test,prediction)*100
-    #precision, recall, f_score = precision_recall_fscore_support(labels_test, prediction, average='weighted')
-    #end =  -1*(start-time.time())
-    #print("Metrics calculated in " + str(end) + " seconds")
+#start = time.time()
+#print("Calculating metrics")
+# Using the model to predict the labels of the test data
+prediction = Classifier.predict(features_test)
 
-    # Printing the results
-    #print("----------------------------------------------------------------------------------------------------")
-    #print("Test Accuracy: ", accuracy)
-    #print ("Precision: ", precision) 
-    #print ("Recall: ", recall) 
-    #print("F-score: ", f_score)
-    #print("----------------------------------------------------------------------------------------------------")
+# Evaluating the accuracy of the model using the sklearn functions
+accuracy = accuracy_score(labels_test,prediction)*100
+precision = precision_score(labels_test,prediction, zero_division=0)*100
+recall = recall_score(labels_test,prediction)*100
+#precision, recall, f_score = precision_recall_fscore_support(labels_test, prediction, average='weighted')
+#end =  -1*(start-time.time())
+#print("Metrics calculated in " + str(end) + " seconds")
 
-    #Save the classifier
-    #joblib.dump(Classifier, "./Classifier.joblib")
+#Generate custom metrics
+start = time.time()
+print("Generating metric data")
+stuff = prediction
+TruePositives=0
+FalseNegatives=0
+FalsePositives = 0
+TrueNegatives = 0
+for i in range(len(stuff)):
+    if(labels_test[i] == 1):
+        if(stuff[i]==1):
+            TruePositives+=1
+        else:
+            FalseNegatives+=1
+    else:
+        if(stuff[i]==1):
+            FalsePositives+=1
+        else:
+            TrueNegatives+=1
+end =  -1*(start-time.time())
+print("Metrics generated in " + str(end) + " seconds")
+print("Test:")
+print(FalseNegatives, "   ", TruePositives)
+print(FalsePositives, "   ", TrueNegatives)
 
-    i+=1
+# Printing the results
+print("----------------------------------------------------------------------------------------------------")
+print("Test Accuracy: ", accuracy)
+print ("Precision: ", precision) 
+print ("Recall: ", recall) 
+#print("F-score: ", f_score)
+print("----------------------------------------------------------------------------------------------------")
 
-    #Example for how to load classifier
-    #loaded_classifier: SVC = joblib.load("./Classifier.joblib")
+#Save the classifier
+joblib.dump(Classifier, "./Classifier (SVM).joblib")
 
-    array[i] = (accuracy, precision, recall)
-    del Classifier, prediction, accuracy, recall, precision
+
+#Example for how to load classifier
+#loaded_classifier: SVC = joblib.load("./Classifier.joblib")
+
     
-print("\n\n")
-i=0
-while i<81:
-    inte = 0.05*i + 53
-    print("Test accuracy, precision, recall, with ratio 0:1, 1: " + str(inte) + " : ", array[i][0], "   ", array[i][1], "    ", array[i][2])
-    i+=1
+
+
 
 TrueEndSeconds = -1*(TrueStart-time.time())
 TrueEndMinutes = (TrueEndSeconds - TrueEndSeconds%60)/60
